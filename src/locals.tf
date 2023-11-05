570a2654297ca67a22411e545303e3802d560800
Daniel Mwanthi <dmwanthi2@gamil.com>
2023-11-05 04:45:18 +0300

    added 'eu-north-1a' and 'eu-north-1b' azs

--- src/locals.tf
+++ src/locals.tf
@@ -1,6 +1,6 @@
 locals {
   vpc_cidr             = "10.0.0.0/16"
-  availability_zones   = ["eu-west-3a", "eu-west-3b"]
+  availability_zones   = ["eu-north-1a", "eu-north-1b"]
   public_subnet_cidrs  = ["10.0.0.0/24", "10.0.1.0/24"]
   private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
 }
